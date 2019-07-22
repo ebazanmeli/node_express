aa051f75f539ccbbfee8a08f0a97b7e9a0413f90
ebazan <ezequiel.bazan@mercadolibre.com>
2019-07-22 15:08:24 -0300

    first commit

---
+++ routes/sites.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var request = require("request");
+var router = express.Router();
+
+/* GET sites listing. */
+router.get('/', function(req, res) {
+    request.get("https://api.mercadolibre.com/sites", function (error, response, body) {
+        if(error) {
+            res.send(error);
+        }
+        res.send(JSON.parse(body));
+    });
+});
+
+/* GET site specific by id. */
+router.get('/:id', function(req, res) {
+    var id = req.params.id;
+    request.get("https://api.mercadolibre.com/sites/" + id, function (error, response, body) {
+        if(error) {
+            res.send(error);
+        }
+        res.send(JSON.parse(body));
+    });
+});
+
+module.exports = router;
